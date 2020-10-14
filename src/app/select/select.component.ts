3ea29229602f792cd5831c226c69281aad6b234d
Lakshya <lakshyadahiya@gmail.com>
2020-10-14 23:08:35 +0530

    Add case to show/hide child lists

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Tree } from '../interfaces';
 
 @Component({
@@ -10,20 +10,14 @@ export class SelectComponent implements OnInit {
 
   @Input() tree: Tree;
   @Output() selected = new EventEmitter<any>();
-  showChildrenBoolean: boolean;
-  constructor(
-    private el: ElementRef
-  ) { }
+  showChild: boolean;
+
+  constructor() { }
 
   ngOnInit(): void { }
 
-  showChildOrSelect(i) {
-
-  }
-
   onSelect(childValue: string[], value: string) {
-    this.el.nativeElement.style.display = 'none';
+    this.showChild = false;
     this.selected.emit([value, ...childValue])
   }
-
 }
